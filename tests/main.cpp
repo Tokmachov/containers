30944426ef04c72f1c708b96fc6d6d0c8979278b
Oleg <oleg.tokmachov@icloud.com>
2021-05-29 19:48:49 +0300

    comparison_ops_base_vector_iterator

--- tests/main.cpp
+++ tests/main.cpp
@@ -2,7 +2,7 @@
 # include "ListIteratorTestCase.hpp"
 # include "ListTestCase.hpp"
 # include "VectorTestCase.hpp"
-
+# include "../vector/VectorIteratorBase.hpp"
 int main(void)
 {
     //ListIteratorTestCase::run();
--- vector/VectorIteratorBase.hpp
+++ vector/VectorIteratorBase.hpp
@@ -1,5 +1,33 @@
 template <typename Value>
 class VectorIteratorBase
 {
-    
-}
+    public:
+        VectorIteratorBase(Value *storagePtr)
+            : _storagePtr(storagePtr) {}
+        bool operator==(const VectorIteratorBase<Value> &rhs)
+        {
+            return _storagePtr == rhs._storagePtr;
+        }
+        bool operator!=(const VectorIteratorBase<Value> &rhs)
+        {
+            return _storagePtr != rhs._storagePtr;
+        }
+        bool operator<(const VectorIteratorBase &rhs)
+        {
+            return _storagePtr < rhs._storagePtr;
+        } 
+        bool operator>(const VectorIteratorBase &rhs)
+        {
+            return _storagePtr > rhs._storagePtr;
+        }
+        bool operator<=(const VectorIteratorBase &rhs)
+        {
+            return _storagePtr <= rhs._storagePtr;
+        }
+        bool operator>=(const VectorIteratorBase &rhs)
+        {
+            return _storagePtr >= rhs._storagePtr;
+        }
+    protected:
+        Value *_storagePtr;
+};
